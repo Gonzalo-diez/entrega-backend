41d690a925f70bcd89b970989fb814183d805a10
Gonzalo <gonzalodiez97@gmail.com>
2024-02-26 01:39:36 -0300

    Instalacion de de handlebars y websocket

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import http from "http";
+import handlebars from "express-handlebars";
+import { Server } from "socket.io"
 import bodyParser from "body-parser";
+import __dirname from "./util.js";
 import productRouter from "./routes/product.router.js";
 import cartRouter from "./routes/cart.router.js";
 
 const app = express();
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
 // Middleware para analizar el cuerpo de la solicitud JSON
 app.use(express.json());
@@ -14,11 +17,24 @@ app.use(express.json());
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 
+// Middleware para utilizar plantillas html
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
 // Middleware adicional para analizar el cuerpo de la solicitud JSON en cartRouter
 cartRouter.use(bodyParser.json());
 
 const PORT = 8080;
 
-server.listen(PORT, () => {
+// Servidor HTTP
+httpServer.listen(PORT, () => {
     console.log("Servidor conectado!!");
 });
+
+// Servidor WebSocket
+const socketServer = new Server(httpServer);
+
+socketServer.on('connection', socket => {
+    console.log("Nuevo cliente conectado!!")
+})
